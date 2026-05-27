f4f2cfa93a798fa0b6cc87e2a8861c1cf50b9b5f
laodongX <jdongap@connect.ust.hk>
2026-05-27 14:18:58 +0800

    Merge

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup, find_packages
-
+with open(file="README.md",mode='r',encoding='utf-8') as fh:
+    long_description = fh.read()
 setup(
     name="fidelity-metrics",
     version="0.1.0",
     author="laodongX",         # 换成你的名字
+    long_description=long_description,
     description="Semantic Fidelity Metrics for LLM/VLM Training: Measuring Information Loss Across Layers",
     packages=find_packages(),
     python_requires=">=3.8",
